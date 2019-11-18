b78204fba04c5e697dfa033ae4b656cef97da906
Ian Sim <e0031628@u.nus.edu>
2019-11-18 11:10:28 +0800

    add file upload

--- src/Components/Dashboard/Upload.js
+++ src/Components/Dashboard/Upload.js
@@ -2,15 +2,31 @@ import React from "react";
 import { Icon } from "antd";
 
 export default function Upload() {
+  const onChangeHandler = ev => {
+    ev.preventDefault();
+
+    const data = new FormData();
+    data.append("file", ev.target.files[0]);
+    //data.append('filename', this.fileName.value);
+
+    fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body: data
+    });
+  };
+
   return (
     <div className="card">
       <div style={{ textAlign: "center" }}>
-        <h1>UPLOAD</h1>
-        <Icon
-          style={{ transform: "scale(2)", marginBottom: "24px" }}
-          type="upload"
-        />
-        <p>Drag file here to upload Invoice</p>
+        <h1>
+          <Icon
+            style={{ transform: "scale(2)", marginBottom: "24px" }}
+            type="upload"
+          />
+          <br />
+          UPLOAD FILE
+        </h1>
+        <input type="file" name="file" onChange={onChangeHandler} />
       </div>
     </div>
   );
